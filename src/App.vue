1f7f67abd4ff9a0e9b448cf361ad7de29c11bcb6
AbdulloxOrifjonov <abdulloxstrike1@gmail.com>
2024-12-20 23:34:50 +0500

    yengilila

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,11 @@ import im from "@/images/i'm.jpg";
 
 import ScrollTrigger from "gsap/ScrollTrigger";
 
-// ScrollTrigger plaginini o‘rnatish
+// ScrollTrigger plugin registration
 gsap.registerPlugin(ScrollTrigger);
 
 onMounted(() => {
-  // Fade-in-left animatsiyasi
+  // Fade-in-left animation
   gsap.utils.toArray(".gsap-fade-in-left").forEach((el) => {
     gsap.fromTo(
       el,
@@ -36,7 +36,7 @@ onMounted(() => {
     );
   });
 
-  // Fade-in-right animatsiyasi
+  // Fade-in-right animation
   gsap.utils.toArray(".gsap-fade-in-right").forEach((el) => {
     gsap.fromTo(
       el,
@@ -55,7 +55,7 @@ onMounted(() => {
     );
   });
 
-  // Fade-in-up animatsiyasi
+  // Fade-in-up animation
   gsap.utils.toArray(".gsap-fade-in-up").forEach((el) => {
     gsap.fromTo(
       el,
@@ -74,6 +74,24 @@ onMounted(() => {
       },
     );
   });
+
+  // Hero image animation (scale and fade-in)
+  gsap.fromTo(
+    ".hero-img",
+    { opacity: 0, scale: 0.9 },
+    {
+      opacity: 1,
+      scale: 1,
+      duration: 1,
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger: ".hero-img",
+        start: "top 80%",
+        end: "top 50%",
+        scrub: true,
+      },
+    },
+  );
 });
 
 const navItems = [
@@ -101,12 +119,12 @@ const navItems = [
     <main>
       <div
         id="home"
-        class="hero py-[100px] px-8 flex flex-col lg:flex-row text-white items-center justify-around w-full"
+        class="hero py-[100px] px-8 flex flex-col lg:flex-row text-white items-center justify-around w-full bg-gray-600"
       >
-        <div class="gsap-fade-in-left w-full lg:w-[50%] text-center lg:text-left mb-6 lg:mb-0">
-          <h2 class="text-4xl font-bold mb-4">
-            Hi, I'm <span class="text-blue-400">Orifjonov Abdulloh</span>
-          </h2>
+        <div
+          class="gsap-fade-in-left w-full lg:w-[50%] flex flex-col justify-start items-start text-left lg:text-left mb-6 lg:mb-0"
+        >
+          <h2 class="text-left font-bold mb-4 text-white text-4xl">Hi, I'm Orifjonov Abdulloh</h2>
           <p class="text-lg text-gray-300 mb-6">
             He crafts responsive websites where technologies meet creativity
           </p>
@@ -118,22 +136,19 @@ const navItems = [
           <img
             :src="im"
             alt="Your Image"
-            class="rounded-full shadow-lg border-4 border-blue-500 w-full"
+            class="hero-img rounded-full shadow-lg border-4 border-blue-500 w-full"
           />
         </div>
       </div>
 
       <!-- About Me Section -->
-      <section id="about_me" class="about-me bg-gray-100 py-16 px-4 sm:px-8">
-        <h2 class="text-4xl font-bold text-center text-gray-800 mb-8">About Me</h2>
-        <div class="max-w-4xl mx-auto bg-white shadow-lg rounded-lg p-6 gsap-fade-in-up">
-          <p class="text-lg text-gray-700 mb-4"><strong>Ism:</strong> Abdullox Orifjonov</p>
-          <p class="text-lg text-gray-700 mb-4"><strong>Tug'ilgan sana:</strong> 15-yanvar, 2008</p>
-          <p class="text-lg text-gray-700 mb-4"><strong>Telefon:</strong> +998 33 011 99 01</p>
-          <p class="text-lg text-gray-700 mb-4">
-            <strong>Email:</strong> abdulloxstrike1@gmail.com
-          </p>
-          <p class="text-lg text-gray-700 mb-4">
+      <section id="about_me" class="about-me bg-gray-600 py-16 px-4 sm:px-8">
+        <h2 class="text-4xl font-bold text-center text-gray-100 mb-8">About Me</h2>
+        <div class="max-w-4xl mx-auto bg-gray-300 shadow-lg rounded-lg p-6 gsap-fade-in-up">
+          <p class="text-lg mb-4"><strong>Ism:</strong> Abdullox Orifjonov</p>
+          <p class="text-lg mb-4"><strong>Yosh:</strong> 17</p>
+          <p class="text-lg mb-4"><strong>Telefon:</strong> +998 33 011 99 01</p>
+          <p class="text-lg mb-4">
             <strong>Telegram: </strong>
             <a
               href="https://t.me/Abdullox99"
@@ -143,95 +158,82 @@ const navItems = [
               >@Abdullox99</a
             >
           </p>
-          <p class="text-lg text-gray-700 mb-4"><strong>Manzil:</strong> Toshkent, Shayhontohur</p>
-          <p class="text-lg text-gray-700 mb-4">
+          <p class="text-lg mb-4"><strong>Manzil:</strong> Toshkent, Shayhontohur</p>
+          <p class="text-lg mb-4">
             <strong>Maqsad:</strong> Senior Frontend Developer sifatida rivojlanish
           </p>
-          <p class="text-lg text-gray-700 mb-4"><strong>Ta'lim:</strong> PDP Academy</p>
-          <p class="text-lg text-gray-700">
+          <p class="text-lg mb-4">
+            <strong>Shaxsiy Xususiyatlar:</strong> Muloqotga ochiq, muammolarni hal qilishga
+            yo'naltirilgan, mas'uliyatli va o'rganishga chanqoq.
+          </p>
+
+          <p class="text-lg mb-4"><strong>Ta'lim:</strong> PDP Academy</p>
+          <p class="text-lg mb-4">
             <strong>Ko'nikmalar:</strong> HTML, CSS, JavaScript, React, Redux, Redux Thunk, Redux
             Toolkit, Context API, Next.js, Vue.js
+          </p>
+
+          <p class="text-lg mb-4">
+            <strong>Til Bilimlari:</strong> O'zbek tili (ona tili), Ingliz tili (30%), Rus tili
+            (70%).
           </p>
         </div>
       </section>
 
       <!-- Portfolio Section -->
-      <section id="skills" class="portfolio py-20 px-4 sm:px-8 bg-gray-500">
+      <section id="skills" class="portfolio py-20 px-4 sm:px-8 bg-gray-600">
         <div>
           <h2 class="text-4xl font-bold text-center mb-8 text-gray-100">Skills</h2>
           <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-            <!-- Balance App -->
-            <div class="bg-gray-100 shadow-lg rounded overflow-hidden gsap-fade-in-up">
-              <img :src="balance" alt="" class="w-full" />
+            <!-- Cards -->
+            <div
+              v-for="(item, index) in [
+                {
+                  img: balance,
+                  title: 'Wallet App',
+                  desc: 'Wallet app with React',
+                  link: 'https://wallet-app1.netlify.app/',
+                },
+                {
+                  img: choco,
+                  title: 'Chocolate App',
+                  desc: 'Chocolate App made with Vue.js',
+                  link: 'https://choco-figma.netlify.app/',
+                },
+                {
+                  img: digital,
+                  title: 'Digital Store',
+                  desc: 'Digital Store web app',
+                  link: 'https://digital-market-figma.netlify.app/',
+                },
+                {
+                  img: yesil,
+                  title: 'Yesil Dan',
+                  desc: 'Agricultural web app',
+                  link: 'https://yesil-dan.netlify.app/',
+                },
+                {
+                  img: ids,
+                  title: 'IDS',
+                  desc: 'Project management app',
+                  link: 'https://ids-projectt.netlify.app/',
+                },
+                {
+                  img: gym,
+                  title: 'Gym App',
+                  desc: 'Gym management app',
+                  link: 'https://landing.gymrat.uz/',
+                },
+              ]"
+              :key="index"
+              class="bg-gray-100 shadow-lg rounded overflow-hidden gsap-fade-in-up hover:scale-105 transform transition duration-300 ease-in-out"
+            >
+              <img :src="item.img" :alt="item.title" class="w-full" />
               <div class="p-4">
-                <h3 class="text-xl font-bold mb-2">Wallet App</h3>
-                <p class="text-gray-600">Wallet app with React</p>
-                <a href="https://wallet-app1.netlify.app/" target="_blank">
+                <h3 class="text-xl font-bold mb-2">{{ item.title }}</h3>
+                <p class="text-gray-600">{{ item.desc }}</p>
+                <a :href="item.link" target="_blank">
                   <button class="bg-blue-500 text-white py-1 px-3 mt-4 rounded">View More</button>
-                </a>
-              </div>
-            </div>
-            <!-- Digital Market -->
-            <div class="bg-gray-100 shadow-lg rounded overflow-hidden gsap-fade-in-up">
-              <img :src="digital" alt="" class="w-full" />
-              <div class="p-4">
-                <h3 class="text-xl font-bold mb-2">Digital Market</h3>
-                <p class="text-gray-600">This website is about digital marketing</p>
-                <a href="https://digital-market-figma.netlify.app/" target="_blank">
-                  <button class="bg-blue-500 text-white py-1 px-3 mt-4 rounded">View More</button>
-                </a>
-              </div>
-            </div>
-            <!-- Choco Market -->
-            <div class="bg-gray-100 shadow-lg rounded overflow-hidden gsap-fade-in-up">
-              <img :src="choco" alt="" class="w-full" />
-              <div class="p-4">
-                <h3 class="text-xl font-bold mb-2">Choco Market</h3>
-                <p class="text-gray-600">Website about chocolate marketing</p>
-                <a href="https://choco-figma.netlify.app/" target="_blank">
-                  <button class="bg-blue-500 text-white py-1 px-3 mt-4 rounded">View More</button>
-                </a>
-              </div>
-            </div>
-            <!-- IDS -->
-            <div class="bg-gray-100 shadow-lg rounded overflow-hidden gsap-fade-in-up">
-              <img :src="ids" alt="" class="w-full" />
-              <div class="p-4">
-                <h3 class="text-xl font-bold mb-2">Web-Site IDS</h3>
-                <p class="text-gray-600">This real project is about IDS company</p>
-                <a href="https://ids-projectt.netlify.app/" target="_blank">
-                  <button class="bg-blue-500 text-white py-1 px-3 mt-4 rounded">View More</button>
-                </a>
-              </div>
-            </div>
-            <div class="bg-gray-100 shadow-lg rounded overflow-hidden gsap-fade-in-up">
-              <img :src="yesil" alt="" class="w-full" />
-              <div class="p-4">
-                <h3 class="text-xl font-bold mb-2">Yesil Dan</h3>
-                <p class="text-gray-600">This website provides information about the flour mill</p>
-                <a href="https://yesil-dan.netlify.app/" target="_blank">
-                  <button
-                    class="bg-blue-500 text-white py-1 px-3 mt-4 rounded hover:bg-blue-400 transition"
-                  >
-                    View More
-                  </button>
-                </a>
-              </div>
-            </div>
-
-            <div class="bg-gray-100 shadow-lg rounded overflow-hidden gsap-fade-in-up">
-              <img :src="gym" alt="" class="w-full" />
-              <div class="p-4">
-                <h3 class="text-xl font-bold mb-2">GYM Rat</h3>
-                <p class="text-gray-600">
-                  The task of this GYM Rat SRM appliaction is to automate the work of Gyms
-                </p>
-                <a href="https://landing.gymrat.uz/" target="_blank">
-                  <button
-                    class="bg-blue-500 text-white py-1 px-3 mt-4 rounded hover:bg-blue-400 transition"
-                  >
-                    View More
-                  </button>
                 </a>
               </div>
             </div>
@@ -239,21 +241,32 @@ const navItems = [
         </div>
       </section>
     </main>
-
-    <!-- Footer -->
-    <footer class="bg-gray-900 text-white py-4 text-center">
-      <p>&copy; 2024 MyPortfolio. All Rights Reserved.</p>
-    </footer>
   </div>
 </template>
 
 <style scoped>
 .hero {
-  background: #2a2a2a;
+  background-image: url("https://example.com/hero-background.jpg");
+  background-size: cover;
+  background-position: center;
+  color: white;
 }
 
-.gsap-fade-in-up {
-  opacity: 0;
-  transform: translateY(50px);
+.hero-img {
+  transition: transform 0.3s ease-in-out;
+}
+
+.hero-img:hover {
+  transform: scale(1.05);
+}
+
+#about_me p {
+  font-size: 1.125rem;
+  color: #333;
+}
+
+#skills .grid div:hover {
+  transform: scale(1.05);
+  transition: transform 0.3s ease-in-out;
 }
 </style>
